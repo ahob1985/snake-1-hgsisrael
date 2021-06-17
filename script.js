53293c15548b71ee1c81560426f2aca405cbd72a
Israel Fleetwood <67342332+hgsisrael@users.noreply.github.com>
2021-06-17 21:04:14 +0000

    almost done

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Author:
 
+
 // Global UI Variables
 let canvasDiv;
 let canvas;
 let textDiv;
-let textp;
+let textP;
 let buttonDiv;
 let resetButton;
 
@@ -12,55 +13,80 @@ let resetButton;
 let snake;
 let food;
 let resolution;
-let scaleWidth;
-let scaleHeight;
+let scaledWidth;
+let scaledHeight;
 let score;
 
 function setup() {
   // Build the interface
-canvasDiv - createDiv();
-canvas = createCanvas(640, 480);
-canvas.parent(canvasDiv);
-textDiv = createDiv();
-textP = createP();
-textP.parent(textDiv);
-buttonDiv = createDiv();
-resetButton = createButton("Reset Game");
-restButton.mousePressed(resetGame);
-resetButton.parent(buttonDiv);
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP();
+  textP.parent(textDiv);
+  buttonDiv = createDiv();
+  resetButton = createButton("Reset Game");
+  resetButton.mousePressed(resetGame);
+  resetButton.parent(buttonDiv);
   // Set the resolution to 20. Play with this later if you want.
-resolution = 20;
+  resolution = 20; 
   // Scaled width and height are width / resolution, height / resolution
-scaledWidth = floor(width / resolution);
-scaledHeight = floor(height / resolution);
+  scaledWidth = floor(width / resolution);
+  scaledHeight = floor(height / resolution);
   // Set the game's framerate to 5 (or whatever you prefer)
   frameRate(5);
-  // Call resetGame() to initialize everything else.
+  // // Call resetGame() to initialize everything else.
   resetGame();
 }
 
 function draw() {
   // Scale the canvas according to resolution, then refresh the background
-
+  scale(resolution);
+  background(220);
   // Check if snake is eating the food
-
+  if(snake.eat(food)) {
+    createFood();
+  }
   // Draw the snake
-snake.update();
-snake.show();
+  snake.update();
+  snake.show();
+
   // Draw the food
-
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
   // Check for game over
-
+  if(snake.endGame()) {
+    textP.html("GAME OVER. Final Score: " + score)
+    background(255, 0, 0);
+    noLoop();
+    buttonDiv.style("display", "block");
+  }
 }
 
 function createFood() {
-
+  let x = floor(random(scaledWidth));
+  let y = floor(random(scaledHeight));
+  food = createVector(x, y);
 }
 
 function keyPressed() {
-
+  if(keyCode === UP_ARROW && snake.yDirection === 0) {
+    snake.setDirection(0, -1);
+  } else if(keyCode === DOWN_ARROW && snake.yDirection === 0) {
+    snake.setDirection(0, 1);
+  } else if(keyCode === LEFT_ARROW && snake.xDirection === 0) {
+    snake.setDirection(-1, 0);
+  } else if(keyCode === RIGHT_ARROW && snake.xDirection === 0) {
+    snake.setDirection(1, 0);
+  }
 }
 
 function resetGame() {
-
+  // Instantiate a new Snake object
+  snake = new Snake();
+  // Create a food object
+  createFood();
 }
+
--- snake.js
+++ snake.js
@@ -1,8 +1,8 @@
 class Snake {
 
   constructor() {
-    this.body = [];
-    this.body[0] = createVector(floor(scaledWidth / 2),floor(scaleHeight / 2));
+    this.body = []; 
+    this.body[0] = createVector(floor(scaledWidth / 2), floor(scaledHeight / 2));
     this.xDirection = 0;
     this.yDirection = 0;
   }
@@ -11,24 +11,35 @@ class Snake {
     let head = this.body[this.body.length - 1].copy();
     this.body.shift();
     head.x += this.xDirection;
-    head.x += this.yDirection;
+    head.y += this.yDirection;
     this.body.push(head);
   }
 
   endGame() {
-
+    let head = this.body[this.body.length - 1];
+    if(head.x > scaledWidth - 1 || head.x < 0 || head.y > scaledHeight - 1 || head.y < 0) {
+      return true;
+    }
+    return false;
   }
 
   grow() {
-
+    let head = this.body[this.body.length - 1].copy();
+    this.body.push(head);
   }
 
   eat(food) {
-
+    let head = this.body[this.body.length - 1];
+    if(head.x === food.x && head.y === food.y) {
+      this.grow();
+      return true;
+    }else {
+      return false;
+    }
   }
 
   show() {
-    for(let i = 0 < this.body.length; i++) {
+    for(let i = 0; i < this.body.length; i++) {
       fill(0);
       noStroke();
       rect(this.body[i].x, this.body[i].y, 1, 1);
